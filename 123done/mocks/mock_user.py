264a26d5c943f7189b67b5f588b6905bf538f74d
Zac <zac@zac-Ubuntu.(none)>
2012-05-23 14:36:47 +0100

    Fix typo.

--- 123done/mocks/mock_user.py
+++ 123done/mocks/mock_user.py
@@ -5,7 +5,7 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 
-class MockApplication(dict):
+class MockUser(dict):
 
     def __init__(self, **kwargs):
         # set your default values
